6545305171924216ab90342c8896f5fc537cb9fc
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-01-14 20:15:05 -0500

    arrPusher, foodImage function, Misc. Changes
    - Added arrPusher function to help with pushing new class objects into
    foodArr
    - Created foodImage function to store an icon of every food (class
    object)
    - Removed unused exports

--- src/main-page.js
+++ src/main-page.js
@@ -1,42 +1,52 @@
 "use strict";
 
-export const container = document.querySelector("#content");
-
 export const foodArr = [];
 
 // creating Food class
 // - contains an ingredients array containing the ingredients for the current food
 export class Food {
-  constructor(name, type, price) {
+  constructor(name, type, price, imgUrl) {
     this.foodName = name;
     this.foodType = type;
     this.foodPrice = price;
+    this.imgUrl = imgUrl;
+  }
+  foodImage(imgUrl, classListName) {
+    this.imgEl = document.createElement("img");
+    this.imgEl.src = imgUrl;
+    this.imgEl.classList.add(classListName);
   }
   foodStr() {
-    console.log(
-      `${this.foodName} is a ${this.foodType} and is currently selling for ${this.foodPrice}`,
-    );
+    return `${this.foodName} is a ${this.foodType} and is currently selling for ${this.foodPrice}`;
   }
   ingredients = [];
 }
 
-const lecheFlan = new Food("Leche Flan", "Desert", "$7.00");
+const lecheFlan = new Food(
+  "Leche Flan",
+  "Desert",
+  "$7.00",
+  "https://cdn-icons-png.flaticon.com/128/8065/8065209.png",
+);
 const tteokbokki = new Food("Tteokbokki", "Meal", "$12.00");
 const sisig = new Food("Sisig", "Meal", "$5.00");
+const takoyaki = new Food("Takoyaki", "Snack", "$2.00");
 
-// TODO: create function to push food to array instead of calling individually
-foodArr.push(lecheFlan);
-foodArr.push(tteokbokki);
-foodArr.push(sisig);
+// TODO: create function to create food objects
+const foodMaker = function (name, type, price) {
+  return new Food(`${name}`, `${type}`, `${price}`);
+};
+console.log(foodMaker("Tacos", "Meal", "$8.00"));
 
-console.log(foodArr);
-
-// function to create divs based on number of objects in foodArr
-const displayItems = function () {
-  foodArr.forEach((element) => {
-    const cell = document.createElement("div");
-    cell.classList.add("food-container");
-    cell.textContent = element.foodName;
-    container.appendChild(cell);
+// array pusher function
+// - takes any array and using rest parameter for foods
+// - creates array of params
+// - looping over that array of params
+// - pushing that current food to whatever arr we pass in
+const arrPusher = function (arr, ...foods) {
+  foods.forEach((food) => {
+    arr.push(food);
   });
 };
+
+arrPusher(foodArr, lecheFlan, tteokbokki, sisig, takoyaki);
