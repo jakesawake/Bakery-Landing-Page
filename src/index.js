290c7809671460a9b67622aa4b19d4662d7d9db5
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-01-17 21:53:57 -0500

    Main Logic
    - created main logic in index.js
    - dynamically displaying page depending on nav element clicked
    - appending content to container based on function call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 "use strict";
 
 import "./css/style.css";
-import { displayItems } from "./display-items.js";
+import { displayMenu } from "./display-menu.js";
+import { displayHome } from "./display-home.js";
+import { displayInquiryForm } from "./display-inquiry.js";
+export const container = document.querySelector("#content");
 
-displayItems();
+// selecting all nav buttons
+const navBtns = document.querySelectorAll(".nav-link");
+
+// function to clear the container
+const clearContent = function () {
+  container.textContent = "";
+};
+
+// calling displayHome() to show home page on initial page load
+displayHome();
+
+// - dynamically looping through the nav elements
+// - listening for click on each nav button
+// - depending on which nav element is clicked -> call the function for the corresponding module
+navBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.id === "Menu-Btn") {
+      clearContent();
+      displayMenu();
+    } else if (e.target.id === "Home-Btn") {
+      clearContent();
+      displayHome();
+    } else if (e.target.id === "Inquiry-Btn") {
+      clearContent();
+      displayInquiryForm();
+    }
+  });
+});
